09aa17eaba0e0762dd55a8e0e70e200e18e3ca91
jotabe99 <jotabe99@9f6eef5a-f22f-0410-8144-57c74dde152f>
2007-10-08 08:13:06 +0000

    Cambios en generacion cache full/update

--- htdocs/cgi-bin/cache/gc-sincro-ch.sh
+++ htdocs/cgi-bin/cache/gc-sincro-ch.sh
@@ -3,15 +3,17 @@
 #
 # Genera cache de sincroguía en XML de 1 canal
 # $1 Fichero sincroguia (.db)
-# $2 horaUTCparrilla
+# $2 Actualizacion = 0 / Generacion completa = 1
+# $3 horaUTCparrilla
 
 # Ejecutamos el script con baja prioridad
 renice 20 $$ > /dev/null
 
 # Obtener parametros
 chID=`echo $1 | cut -d"_" -f2 | cut -d"." -f1`
-if [ "Z$2" != "Z" ]; then
-	horaUTCparrilla=$2
+full_cache=$2
+if [ "Z$3" != "Z" ]; then
+	horaUTCparrilla=$3
 else
 	horaUTCparrilla=`date +%s`
 fi
@@ -37,70 +39,85 @@ if [ -f $1 ]; then
 	# Log del proceso
 	printf "`date` Canal [%3s]: " "$chID" >> $LOG
 
-	# Copia temporal de sincroguia
-	DB_FILE=/tmp/`basename $1`
-	cp $1 $DB_FILE
-
-	# Extraer datos
-	if [ $? -eq 0 -a -f $DB_FILE ]; then
-		# Generar ficheros vacios
-		echo -n "" > ${CACHE_FILE}.text
-		echo -n "" > ${CACHE_FILE}.xml
-		echo -n "" > ${CACHE_FILE}.html
-	
-		# Log del proceso
-		echo -n "Generación " >> $LOG
-	
-		# Generar fichero text
-		echo -n "db->text" >> $LOG
-		www-tools db2text $DB_FILE > ${CACHE_FILE}.text.temp
-		ST_db2text=$?
-		if [ $ST_db2text -ne 0 ]; then
-			echo " <b>ERROR</b>" >> $LOG
-		else
-			# Ordenar fichero text
-			sort ${CACHE_FILE}.text.temp > ${CACHE_FILE}.text
-	
-			# Generar fichero xml
-			echo -n ",text->xml" >> $LOG
-			echo "<CHANNEL cid=\"$cid\" id=\"${chID}\" name=\"$chName\" file=\"$1\" numChannel=\"$numChannel\">" >> ${CACHE_FILE}.xml
-			www-tools text2xml ${chID} ${CACHE_FILE}.text >> ${CACHE_FILE}.xml
-			ST_text2xml=$?
-			echo "</CHANNEL>" >> ${CACHE_FILE}.xml
-			if [ $ST_text2xml -ne 0 ]; then
-				echo " <b>ERROR</b>" >> $LOG
-			else
-				# Comprobar si mostrar imagenes
-				if [ "$MOSTRAR_MINI_IMG" != "si" ]; then
-					MOSTRAR_IMG=0
-				else
-					# Comprobar si obtener imagen de internet
-					if [ "Z$OBTENER_IMG_INET" = "Zsi" ]; then
-						MOSTRAR_IMG=2
-					else
-						MOSTRAR_IMG=1
-					fi
-				fi
-
-				# Generar fichero html
-				echo -n ",text->html" >> $LOG
-				www-tools text2html ${chID} ${CACHE_FILE}.text ${horaUTCparrilla} ${MOSTRAR_IMG} >> ${CACHE_FILE}.html
-				ST_text2html=$?
-				if [ $ST_text2html -ne 0 ]; then
-					echo " <b>ERROR</b>" >> $LOG
-				else
-					# Resultado generacion OK
-					echo " = OK" >> $LOG
-				fi
-			fi
-		fi
+	# Comprobar si debemos actualizar el canal
+	if [ $full_cache -eq 1 ]; then
+		found=1
 	else
-		echo " <b>ERROR</b> copia fichero datos" >> $LOG
+		if [ $1 -nt ${CACHE_FILE}.text ]; then
+			found=1
+		else
+			found=0
+			echo "Sin cambios" >> $LOG
+		fi
 	fi
 
-	# Eliminar temporales
-	rm -f ${CACHE_FILE}.text.temp
-    rm -f $DB_FILE
+	# Actualizar canal
+	if [ $found -eq 1 ]; then
+		# Copia temporal de sincroguia
+		DB_FILE=/tmp/`basename $1`
+		cp $1 $DB_FILE
+	
+		# Extraer datos
+		if [ $? -eq 0 -a -f $DB_FILE ]; then
+			# Generar ficheros vacios
+			echo -n "" > ${CACHE_FILE}.text
+			echo -n "" > ${CACHE_FILE}.xml
+			echo -n "" > ${CACHE_FILE}.html
+		
+			# Log del proceso
+			echo -n "Generación " >> $LOG
+		
+			# Generar fichero text
+			echo -n "db->text" >> $LOG
+			www-tools db2text $DB_FILE > ${CACHE_FILE}.text.temp
+			ST_db2text=$?
+			if [ $ST_db2text -ne 0 ]; then
+				echo " <b>ERROR</b>" >> $LOG
+			else
+				# Ordenar fichero text
+				sort ${CACHE_FILE}.text.temp > ${CACHE_FILE}.text
+		
+				# Generar fichero xml
+				echo -n ",text->xml" >> $LOG
+				echo "<CHANNEL cid=\"$cid\" id=\"${chID}\" name=\"$chName\" file=\"$1\" numChannel=\"$numChannel\">" >> ${CACHE_FILE}.xml
+				www-tools text2xml ${chID} ${CACHE_FILE}.text >> ${CACHE_FILE}.xml
+				ST_text2xml=$?
+				echo "</CHANNEL>" >> ${CACHE_FILE}.xml
+				if [ $ST_text2xml -ne 0 ]; then
+					echo " <b>ERROR</b>" >> $LOG
+				else
+					# Comprobar si mostrar imagenes
+					if [ "$MOSTRAR_MINI_IMG" != "si" ]; then
+						MOSTRAR_IMG=0
+					else
+						# Comprobar si obtener imagen de internet
+						if [ "Z$OBTENER_IMG_INET" = "Zsi" ]; then
+							MOSTRAR_IMG=2
+						else
+							MOSTRAR_IMG=1
+						fi
+					fi
+	
+					# Generar fichero html
+					echo -n ",text->html" >> $LOG
+					www-tools text2html ${chID} ${CACHE_FILE}.text ${horaUTCparrilla} ${MOSTRAR_IMG} >> ${CACHE_FILE}.html
+					ST_text2html=$?
+					if [ $ST_text2html -ne 0 ]; then
+						echo " <b>ERROR</b>" >> $LOG
+					else
+						# Resultado generacion OK
+						echo " = OK" >> $LOG
+					fi
+				fi
+			fi
+		else
+			echo " <b>ERROR</b> copia fichero datos" >> $LOG
+		fi
+	
+		# Eliminar temporales
+		rm -f ${CACHE_FILE}.text.temp
+	    rm -f $DB_FILE
+	fi
 fi
 
 # Eliminar marca de generacion de cache de canal
--- htdocs/cgi-bin/cache/gc-sincro.sh
+++ htdocs/cgi-bin/cache/gc-sincro.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-# pepper, jotabe, (c) Grupo SIESTA, 26-09-2007
+# pepper, jotabe, (c) Grupo SIESTA, 04-10-2007
 #
-# Generacion de cache de todos los canales
-# $1 Lista opcional de canales a actualizar (formato TV3:TV1:...: / siempre termina con ":")
-# Sin parametro se actualizan todos los canales
+# Generacion de cache de sincroguía
+# $1 Tipo de generacion / Lista de canales
+#    full         -> Generacion completa (por defecto)
+#    update       -> Actualizacion todos los canales
+#    TV3:TV1:...: -> Actualizacion lista de canales (siempre termina con ":")
 
 # Ejecutamos el script con baja prioridad
 renice 20 $$ > /dev/null
 
 # Obtener parametros
-numParams=$#
-params=$1
+case $1 in
+	full )
+		full_cache=1;lst_ch="";;
+	update )
+		full_cache=0;lst_ch="";;
+	*:* )
+		full_cache=0;lst_ch=$1;;
+	* )
+		full_cache=1;lst_ch="";;
+esac
 
 # Configurar entorno
 source ../www-setup.shi
@@ -27,17 +37,19 @@ horaUTCparrilla=`date +%s`
 touch ${LCK_SINCRO}
 
 # Guardar horaUTCparrilla de generacion de la parrilla
-if [ $numParams -eq 0 ]; then
+if [ $full_cache -eq 1 ]; then
 	echo "$horaUTCparrilla" > ${Cache}/horaUTCparrilla.txt
 else
-	if [ -f ${Cache}/horaUTCparrilla.txt ]; then
-		horaUTCparrilla=`cat ${Cache}/horaUTCparrilla.txt`
-	fi
+	[ -f ${Cache}/horaUTCparrilla.txt ] && horaUTCparrilla=`cat ${Cache}/horaUTCparrilla.txt`
 fi
 
 # Log del proceso
 utc_inicio=`date +%s`
-echo "`date` Inicio generación XML de Sincroguía [host: `hostname`], [Hora UTC parrilla: $horaUTCparrilla]" > $LOG
+if [ $full_cache -eq 1 ]; then
+	echo "`date` Inicio generación XML de Sincroguía [host: `hostname`], [Hora UTC parrilla: $horaUTCparrilla]" > $LOG
+else
+	echo "`date` Inicio actualización XML de Sincroguía [host: `hostname`], [$*]" >> $LOG
+fi
 echo -n "" > $ERR
 
 # Obtenemos lista de canales
@@ -49,11 +61,18 @@ for Sincrofile in $ListaCanales; do
 	chID=`echo "$Sincrofile" | cut -d"_" -f2 | cut -d"." -f1`
 
 	# Comprobar si canal esta en la lista de canales a generar
-	found=1
-	[ $numParams -eq 1 ] && found=`echo $params | grep "$chID:" | wc -l`
+	if [ $full_cache -eq 1 ]; then
+		found=1
+	else
+		if [ ${#lst_ch} -eq 0 ]; then
+			found=1
+		else
+			found=`echo $lst_ch | grep "$chID:" | wc -l`
+		fi
+	fi
 
 	# Generar cache
-	[ $found -eq 1 ] && ./gc-sincro-ch.sh $Sincrofile $horaUTCparrilla >> $LOG 2>> $ERR
+	[ $found -eq 1 ] && ./gc-sincro-ch.sh $Sincrofile $full_cache $horaUTCparrilla >> $LOG 2>> $ERR
 done
 
 # Eliminar marcas de generacion de cache XML
